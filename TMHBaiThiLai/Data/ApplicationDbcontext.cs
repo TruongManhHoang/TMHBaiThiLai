bb27a93f202e42851ba895dde1a59d5b5559dc99
TruongManhHoang <truongmanhhoang481@gmail.com>
2024-06-20 18:32:34 +0700

    commit first code

---
+++ TMHBaiThiLai/Data/ApplicationDbcontext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TMHBaiThiLai.Models;
+
+namespace MvcMovie.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    // public DbSet<Person> Persons { get; set;}
+    // public DbSet<Employee> Employees { get; set;}
+
+}
---
+++ TMHBaiThiLai/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TMHBaiThiLai.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
